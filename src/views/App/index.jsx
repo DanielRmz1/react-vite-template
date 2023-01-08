25f52e99c3cc478f48160d4700217e184b1765e4
DanielRmz1 <daniel.1912@hotmail.com>
2023-01-08 12:37:59 -0600

    Add relative paths to project

--- src/views/App/index.jsx
+++ src/views/App/index.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import styles from "./styles.module.scss";
+import { Button } from "stories/Button";
 
 const App = () => {
-	return <div className={styles.app}>Welcome</div>;
+	return (
+		<div className={styles.app}>
+			Welcome <svg path={"svg"} />
+			<Button />
+		</div>
+	);
 };
 
 export default App;
